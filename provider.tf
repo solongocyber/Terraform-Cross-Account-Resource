77a8768d2790d0bed786ac613918cbb8e2486ec8
EC2 Default User <ec2-user@ip-172-31-33-174.us-east-2.compute.internal>
2022-10-27 20:29:44 +0000

    provider.tf

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 
   assume_role {
-    role_arn = "arn:aws:iam::055670655228:role/CrossAccountIAMRole"
+    role_arn = "cross_account_role_arn_here"
   }
 
 }
